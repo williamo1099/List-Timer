cd0920d24fd3cec4d8eec2ad768ab863de52b0ce
William Oktavianus <williamoktavianus1099@gmail.com>
2023-07-22 18:15:27 +0700

    Adding list view
    Add a new collection list to the main view.

---
+++ lib/views/collection_detail_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CollectionDetailView extends StatelessWidget {
+  const CollectionDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello"),
+    );
+  }
+}
--- lib/views/collection_list_view.dart
+++ lib/views/collection_list_view.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:list_timer/views/collection_detail_view.dart';
 
 // VIEWS
 import 'package:list_timer/views/widgets/drawer_view.dart';
+
+// MODELS
+import 'package:list_timer/models/collection_model.dart';
+import 'package:list_timer/models/item_model.dart';
+
+final collectionList = [
+  Collection(title: "Running", itemsList: [
+    Item(title: "Run", duration: 10),
+    Item(title: "Walk", duration: 10),
+    Item(title: "Run", duration: 10),
+  ]),
+  Collection(title: "Studying", itemsList: [
+    Item(title: "Study", duration: 30),
+    Item(title: "Rest", duration: 10),
+  ]),
+];
 
 class CollectionListView extends StatelessWidget {
   const CollectionListView({super.key});
 
   void _addNewCollection() {}
+
+  void _viewCollectionDetail(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const CollectionDetailView(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +47,20 @@ class CollectionListView extends StatelessWidget {
 
       // DRAWER
       drawer: DrawerView(),
+
+      // BODY
+      body: ListView.builder(
+        itemCount: collectionList.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(collectionList[index].title),
+          subtitle: Text("10 minutes"),
+          trailing: Text(
+              "${collectionList[index].itemsList.length.toString()} items"),
+          onTap: () {
+            _viewCollectionDetail(context);
+          },
+        ),
+      ),
     );
   }
 }
