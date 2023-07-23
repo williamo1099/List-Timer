72c500cd5fab858c41431cda44072fa337fb0e25
William Oktavianus <williamoktavianus1099@gmail.com>
2023-07-23 12:18:44 +0700

    Updating add collection view
    Update the add collection view, adding new dynamic list view.

--- lib/views/collection_add_view.dart
+++ lib/views/collection_add_view.dart
@@ -8,6 +8,23 @@ class CollectionAddView extends StatefulWidget {
 }
 
 class _CollectionAddViewState extends State<CollectionAddView> {
+  final _formKey = GlobalKey<FormState>();
+
+  final List _itemsList = [true];
+
+  final _titleController = TextEditingController();
+
+  void _addNewCollection() {
+    _formKey.currentState!.validate();
+    _formKey.currentState!.save();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,35 +35,93 @@ class _CollectionAddViewState extends State<CollectionAddView> {
 
       // BODY
       body: Form(
-        child: Column(
-          children: [
-            // TITLE TEXT FORM FIELD
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text("Title"),
+        // KEY
+        key: _formKey,
+
+        // CHILD
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 30, left: 10, right: 10),
+          child: Column(
+            children: [
+              // TITLE TEXT FORM FIELD
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  label: Text("Title"),
+                ),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length >= 20) {
+                    return "Title must be between one and 20 characters long.";
+                  }
+
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value == null ||
-                    value.isEmpty ||
-                    value.trim().length >= 20) {
-                  return "Title must be between one and 20 characters long.";
-                }
 
-                return null;
-              },
-            ),
+              // ITEM LIST VIEW
+              Expanded(
+                child: ListView.separated(
+                  itemCount: _itemsList.length,
+                  itemBuilder: (context, index) => Row(
+                    children: [
+                      // ITEM TITLE TEXT FORM FIELD
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text("Item"),
+                          ),
+                        ),
+                      ),
 
-            //
-            const SizedBox(
-              height: 12,
-            ),
+                      const SizedBox(width: 20),
 
-            // ADD BUTTON
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Add"),
-            ),
-          ],
+                      // ITEM DURATION TEXT FORM FIELD
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text("Duration"),
+                          ),
+                        ),
+                      ),
+
+                      // ADD ICON BUTTON
+                      IconButton(
+                        onPressed: () {
+                          if (index == _itemsList.length - 1) {
+                            setState(() {
+                              _itemsList.insert(index, true);
+                            });
+                          } else {
+                            setState(() {
+                              _itemsList.removeAt(index);
+                            });
+                          }
+                        },
+                        icon: index == _itemsList.length - 1
+                            ? const Icon(Icons.add_circle)
+                            : const Icon(Icons.remove_circle),
+                      ),
+                    ],
+                  ),
+                  separatorBuilder: (context, index) => const SizedBox(
+                    height: 20,
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 12,
+              ),
+
+              // ADD BUTTON
+              ElevatedButton(
+                onPressed: _addNewCollection,
+                child: const Text("Add"),
+              ),
+            ],
+          ),
         ),
       ),
     );
